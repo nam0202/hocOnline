de50696bb1f0c12cd4369e739e8829aa778e983b
nam <namph@ows.com.vn>
2019-02-05 21:00:53 +0700

    update api

--- server/extensions/rest-authen/triggers/checkToken.js
+++ server/extensions/rest-authen/triggers/checkToken.js
@@ -1,15 +1,37 @@
 const jwt = require('jsonwebtoken');
+const acl = require('express-acl');
+acl.config({
+    fileName:'nacl.json',
+    baseUrl:'/api/'
+});
 
 function verifyToken(req, res, next) {
     const token = req.headers['authorization'];
-    if (!token)
-        return res.status(403).send({ auth: false, message: 'No token provided.' });
-    jwt.verify(token.split(" ")[1], 'Bearer', (err, decoded) => {
-        if (err) {
-            return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
-        }else {
-            next(req,res);
-        }
-    });
+    if (!token){
+        acl.authorize(req,res,(err)=>{
+            if(err){
+
+            }else{
+                next(req,res);
+            }
+        })
+    }else{
+        jwt.verify(token.split(" ")[1], 'Bearer', (err, decoded) => {
+            if (err) {
+                return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
+            }else {
+                req.decoded = decoded;
+                console.log(req.decoded);
+                acl.authorize(req,res,function (err) {
+                    if(err){
+                        console.log(err);
+                    }else{
+                        console.log('helloooo');
+                        next(req,res);
+                    }
+                })
+            }
+        });
+    }
 }
 module.exports = verifyToken;
